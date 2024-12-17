42b699f0e35d678bd51f2a21c68c890e987aa04a
Edward Shao <edwardxzshao@gmail.com>
2024-12-17 00:37:52 -0500

    removed unused import

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional, List
 import google.generativeai as genai
